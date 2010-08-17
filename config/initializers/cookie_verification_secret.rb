59faba0d2eb650e180e60a3c54c8084d2bc077c2
Salvatore Giudice <toregiudice@gmail.com>
2010-08-17 01:15:23 -0400

    clearing git repo for hobo

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '3313c6ff21100b3bb3424fc413a99924bc1dd5ec1da79146c05da1c23e4e2252ba5db636b6e599977db6a5de8c147bc4889d84f331c3e676e189f1665979c1b8';
